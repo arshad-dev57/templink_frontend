072304b5a451444bd58a7313b754a2e2c725a173
arshad-dev57 <qureshiarshad947@gmail.com>
2026-09-23 17:51:28 +0500

    worked on the side of

--- lib/Global_Screens/Chat_Screen.dart
+++ lib/Global_Screens/Chat_Screen.dart
@@ -1094,11 +1094,10 @@ class _ChatScreenState extends State<ChatScreen> with WidgetsBindingObserver {
           ),
         ],
       ),
-      // actions: [
-      //   IconButton(onPressed: _startVoiceCall, icon: Icon(Icons.phone_rounded, color: primary), tooltip: 'Voice Call'),
-      //   IconButton(onPressed: _startVideoCall, icon: Icon(Icons.videocam_rounded, color: primary), tooltip: 'Video Call'),
-      // //   IconButton(onPressed: () {}, icon: Icon(Icons.more_vert, color: primary)),
-      // ],
+      actions: [
+        IconButton(onPressed: _startVoiceCall, icon: Icon(Icons.phone_rounded, color: primary), tooltip: 'Voice Call'),
+        IconButton(onPressed: _startVideoCall, icon: Icon(Icons.videocam_rounded, color: primary), tooltip: 'Video Call'),
+      ],
     );
   }
 
